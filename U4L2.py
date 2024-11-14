b4d89923c1d3ba5c21be22b9c8adf7114518a117
MN993647 <993647@pdsb.net>
2024-11-14 14:16:07 -0500

    Update U4L2.py

--- U4L2.py
+++ U4L2.py
@@ -1,29 +1,29 @@
 import turtle
 
 def modify(ln):
-  # Strips everything except the essential string
-  # ln = one line of data from first column to the carriage return
-  #
   mod_string = ""
-  badChars = ['"', ','] # chars to remove
-  ln = ln.strip() # clean out non-printable chars
+  badChars = ['"', ','] 
+  ln = ln.strip() 
   for c in ln:
     if c not in badChars:
       mod_string = mod_string + c
-  return mod_string # line of data should now be clean
+  return mod_string 
 
-def modify1(ln):
+def symbols(ln):
   Rfile = []
   mod_string = ""
-  badChars = ['"', ','] 
+  badChars = ['"', ',', ' '] 
   ln = ln.strip()
   for c in ln:
     if c not in badChars:
       mod_string = c
       Rfile.append(mod_string)
-  return Rfile # line of data should now be clean
+    elif c == ' ':
+      mod_string = '\\'
+      Rfile.append(mod_string)
+  return Rfile 
 
-fh = open("smiley_emoji_mod.xpm", "r") # open a file handle
+fh = open("smiley_emoji_mod.xpm", "r") 
 color_data = fh.readline()
 color_data = modify(color_data)
 [cols, rows, colors] = color_data.split()
@@ -31,19 +31,18 @@ rows = int(rows)
 cols = int(cols)
 colors = int(colors)
 
-colorData = {} # a dictionary of symbols and colors
+colorData = {} 
 
 for i in range(colors):
   cLine = fh.readline()
   cLine = modify(cLine)
   [sym, c, color] = cLine.split()
-  colorData[sym] = color # add a new dictionary entry
-print(colorData)
+  colorData[sym] = color 
 
 def writeDot(obj, rad, color):
-    obj.pendown()
-    obj.dot(rad, color)
-    obj.penup()
+  obj.pendown()
+  obj.dot(rad, color)
+  obj.penup()
 
 t = turtle.Turtle()
 turtle.bgcolor("gray40")
@@ -57,19 +56,21 @@ t.forward(rows//2)
 t.right(90)
 t.pendown()
 
-for y in range(cols):
-    xmp = [0]*cols
-    if y > (colors):
-        fl = fh.readline()
-        fl = modify1(fl)
-        xmp.append(fl)
-        for x in range(rows):
-            writeDot(t, 4, colorData[xmp[x]])
-            t.penup()
-            t.forward(1)
-        t.forward(-1)
-        t.forward(-1*cols)
-        t.right(90)
-        t.forward(2)
-        t.left(90)
-
+for y in range(rows):
+  fl = fh.readline()
+  xmp = symbols(fl)
+  for x in range(cols):
+    if xmp[x] in colorData:
+      writeDot(t, 4, colorData[xmp[x]])
+      t.forward(1)
+    else:
+      print(f"Warning: Undefined symbol '{xmp[x]}' at row {y}, col {x}")
+  t.forward(-1*(rows-1))
+  t.right(90)
+  t.forward(1)
+  t.left(90)
+print("The number of rows:", rows)
+print("The number of columns:", cols)
+print("The number of colors:", colors)
+for color in colorData.values():
+  print(color)
