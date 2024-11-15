570d286e782c3efeb26cf8512dca42b7412147fc
MN993647 <993647@pdsb.net>
2024-11-15 00:14:44 -0500

    Update U4L2.py

--- U4L2.py
+++ U4L2.py
@@ -9,7 +9,7 @@ def modify(ln):
       mod_string = mod_string + c
   return mod_string 
 
-def symbols(ln):
+def symbols(ln, file):
   Rfile = []
   mod_string = ""
   badChars = ['"', ',', ' '] 
@@ -18,12 +18,27 @@ def symbols(ln):
     if c not in badChars:
       mod_string = c
       Rfile.append(mod_string)
-    elif c == ' ':
+    elif c == ' ' and file == "teapot_mod.xpm":
+      mod_string = '~'
+      Rfile.append(mod_string)
+    elif c == ' ' and file == "smiley_emoji_mod.xpm":
       mod_string = '\\'
       Rfile.append(mod_string)
   return Rfile 
 
-fh = open("smiley_emoji_mod.xpm", "r") 
+def writeDot(obj, rad, color):
+  obj.pendown()
+  obj.dot(rad, color)
+  obj.penup()
+  
+usr = input("Enter 1 for smiley emoji, 2 for cool emoji, and 3 for teapot: ")
+usr = int(usr)
+if usr == 1:
+  f = "smiley_emoji_mod.xpm"
+if usr == 3:
+  f = "teapot_mod.xpm"
+  
+fh = open(f, "r") 
 color_data = fh.readline()
 color_data = modify(color_data)
 [cols, rows, colors] = color_data.split()
@@ -39,11 +54,6 @@ for i in range(colors):
   [sym, c, color] = cLine.split()
   colorData[sym] = color 
 
-def writeDot(obj, rad, color):
-  obj.pendown()
-  obj.dot(rad, color)
-  obj.penup()
-
 t = turtle.Turtle()
 turtle.bgcolor("gray40")
 turtle.tracer(0, 0)
@@ -58,14 +68,14 @@ t.pendown()
 
 for y in range(rows):
   fl = fh.readline()
-  xmp = symbols(fl)
-  for x in range(cols):
-    if xmp[x] in colorData:
-      writeDot(t, 4, colorData[xmp[x]])
+  xpm = symbols(fl, f)
+  for x in range(len(xpm)):
+    if xpm[x] in colorData:
+      writeDot(t, 4, colorData[xpm[x]])
       t.forward(1)
     else:
-      print(f"Warning: Undefined symbol '{xmp[x]}' at row {y}, col {x}")
-  t.forward(-1*(rows-1))
+      print(f"Warning: Undefined symbol '{xpm[x]}' at row {y}, col {x}")
+  t.forward(-1*(len(xpm)))
   t.right(90)
   t.forward(1)
   t.left(90)
