f19b092773cc1dcc3d417924209a84ad242cc539
MN993647 <993647@pdsb.net>
2024-11-28 10:37:29 -0500

    Update Assignment2-Polygon.py

--- Assignment2-Polygon.py
+++ Assignment2-Polygon.py
@@ -1,25 +1,27 @@
 import math
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 class Point:
-   def __init__(self, x, y):
-      self.__x = x
-      self.__y = y
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+    def add_point(self, data):
+        new_point = Node(data)
+        if tail is None:
+            tail = new_point
+            new_point.next = new_point
+        else:
+            new_point.next = tail.next
+            tail.next = new_point
+            tail = new_point
+        return tail
 
-   def distance(self, p2):
-      return math.sqrt((self.__x-p2.x)**2 + (self.__y-p2.y)**2)
-    
-ups = input("Enter the coordinates of your polygon in order with the format: a b, x y, ...")
-ups.cleanup()
-#a2.txt = file
-#driver.py = driver file
-#class polygon in poly.py
-#It should be instantiated to some object variable with the following call: myPoly = Polygon()
-#points are added using myPoly.addPoint(x, y) until the past point is entered. 
-#where myPoly is an instance of Polygon()
-#myPoly creates an array of Point() objects
-#A call to compute area (myPoly.area()) should return floating point.
-#similarly, the perimeter (myPoly.perimeter()) should also return floating point.
+    def distance(self, p2):
+        return math.sqrt((self.__x-p2.x)**2 + (self.__y-p2.y)**2)
+class Polygon():
+        
 
-triangle = Polygon()
-triangle.add_point(1, 1)
-triangle.add_point(1, 2)
-triangle.add_point(2, 2)
+        
+        
