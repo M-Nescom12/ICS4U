2662086a82a99f06fe534e9e1a8d721c6c95d004
MN993647 <993647@pdsb.net>
2024-12-17 12:43:29 -0500

    Update poly2.py

--- poly2.py
+++ poly2.py
@@ -10,7 +10,8 @@ class point:
         self.__x = x  # x-coordinate of the point (type: float or None)
         self.__y = y  # y-coordinate of the point (type: float or None)
         self.next = None  # Pointer to the next point in the circular linked list (type: Point or None)
-
+        self.valid()
+        
     def valid(self):
         # PreConditions and Purpose: Validate the x and y coordinates to ensure they are numbers.
         # PostConditions: Converts x and y to int or float (tries before int before float), or exits if invalid.
@@ -25,9 +26,10 @@ class point:
             except ValueError:
                 print("x and y coordinates must be numbers")
                 exit()
+                
     def distance(self, p2):
-        [x2,y2] = p2
-        return math.sqrt((self.__x-x2)**2 + (self.__y-y2)**2)
+        [x2,y2] = p2.get_coordinates()
+        return (math.sqrt((self.__x - x2)**2 + (self.__y - y2)**2))
     
     def get_coordinates(self):
         # PreConditions and Purpose: Retrieve the x and y coordinates of the Point.
@@ -47,8 +49,8 @@ class Polygon:
         self.__sides = 0  # Number of sides in the polygon (type: int)
         self.__vertices = 0  # Number of vertices in the polygon (type: int)
         self.__head = None  # Head node of the circular linked list (type: Point or None)
-        self.__head_angle = 0
-
+        self.__side_lens = []
+        self.__angles = []
 
     def add_point(self, x: float, y: float):
         # PreConditions: Add a new point (x, y) to the polygon as part of a circular linked list.
@@ -73,44 +75,23 @@ class Polygon:
         self.__vertices = self.__vertices + 1
         if self.__vertices > 1:
             self.__sides = self.__vertices
-    
-    def point_dist(self):
-        side_lens = []
-        side = self.__head
-        [x1, y1] = side.get_coordinates()
-        [x2, y2] = side.next.get_coordinates()
-        dist = math.sqrt(((x2 - x1)**2) + ((y2 - y1)**2))
-        side_lens.append(dist)
-        side = side.next
-        while side != self.__head:
-            [x1, y1] = side.get_coordinates()
-            [x2, y2] = side.next.get_coordinates()
-            dist = math.sqrt(((x2 - x1)**2) + ((y2 - y1)**2))
-            side_lens.append(dist)
-            side = side.next
-        return side_lens
-    
+            
     def perimeter(self):
         Sum = 0
-        side_lens = []
         side = self.__head
-        [x,y] = side.get_coordinates()
-        s = point(x,y)
-        dist = point.distance(s)
-        side_lens.append(dist)
+        dist = side.distance(side.next)
+        self.__side_lens.append(dist)
         side = side.next
         while side != self.__head:
-            [x, y] = side.get_coordinates()
-            s = point(x,y)
-            dist = point.distance(s)
-            side_lens.append(dist)
+            dist = side.distance(side.next)
+            self.__side_lens.append(dist)
             side = side.next
-        for x in side_lens:
-            Sum = Sum + side_lens[x]
+        #print(side_lens)
+        for x in self.__side_lens:
+            Sum = Sum + x
         return Sum
     
     def internal_angle(self):
-        angles = []
         vert = self.__head
         [x1, y1] = vert.get_coordinates()
         [x2, y2] = vert.next.get_coordinates()
@@ -137,32 +118,49 @@ class Polygon:
                         self.__head_angle = self.__head_angle - 90
                 else:
                     self.__head_angle = 0
+                    
             if vert == self.__head.next.next:
-                angles.append(theta2)
+                self.__angles.append(theta2)
             [x1, y1] = vert.get_coordinates()
             [x2, y2] = vert.next.get_coordinates()
             [x3, y3] = vert.next.next.get_coordinates()
             dotV = float((x1-x2)*(x3-x2)) + ((y1-y2)*(y3-y2))  
             mag_sum = abs(math.sqrt(((x1 - x2)**2) + ((y1 - y2)**2)))*(math.sqrt(((x3 - x2)**2) + ((y3 - y2)**2)))
+            
             if mag_sum != 0:
                 theta = (math.acos((dotV/mag_sum)))*(180/math.pi)
-                angles.append(theta)
+                self.__angles.append(theta)
                 vert = vert.next
             else:
                 print("No duplicate points...")
                 exit()
-        return angles
+        return self.__angles
     
     def area(self):
-        if _ == . and _ == .:
-            Area = (self.__side(side_lens[0])**2)/(4(math.tan(180/self.__side)))
-            return Area
+        check = 0
+        while check != 1:   
+            for i in range((len(self.__side_lens))-1):
+                if self.__side_lens[i] == self.__side_lens[i+1]:
+                    for i in range((len(self.__angles))-1):
+                        if self.__angles[i] == self.__angles[i+1]:
+                            check = 2
+                        else:
+                            check = 1
+                    if i == range((len(self.__side_lens))-2):
+                         break
+                else:
+                    check = 1
+        if check == 2:
+            n = self.__side
+            s = self.__side_lens[0]
+            Area = (n*((s)**2))/(4*(math.tan((180/n))))
         else:
-            
-        
+            Area = 0
+        return Area
+    
     def drawPoly(self):
         turtle.shape("circle")
-        turtle.shapesize(0.1,0.1)
+        turtle.shapesize(0.3,0.3)
         turtle.speed(1)
         turtle.penup()
         V = self.__head
@@ -187,12 +185,11 @@ class Polygon:
         # Variable dictionary:
         # - nodes (list): List of point strings.
         # - V (Point): Traversal pointer to traverse the circular linked list.
-
-        nodes = []
         V = self.__head 
-        nodes.append(str(V))  # Appending the head point to nodes
+        nodes = str(V)  # Appending the head point to nodes
         V = V.next # Moving to the next node from the head of the lsit
         while V != self.__head:
-            nodes.append(str(V))
+            nodes += " -> "
+            nodes += str(V)
             V = V.next
-        return " -> ".join(nodes) 
+        return nodes 
