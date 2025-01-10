ca678c1c2a7dbd32c77f1ac0f4277ee5150b9a11
MN993647 <993647@pdsb.net>
2025-01-10 10:47:25 -0500

    Update IVT.py

--- IVT.py
+++ IVT.py
@@ -1,3 +1,28 @@
+class Polynomial:
+    def __init__(self, func):
+        self.__func = func
+        self.__len = len(func)
+        self.__EQ = ""
+        
+    def polycraft(self):
+        i = 0
+        for c in self.__func:
+            if self.__len-1 == i:
+                self.__EQ = self.__EQ + str(c)
+                print(c)
+            elif (self.__len -2) == i:
+                piece = str(c) + 'x' 
+                self.__EQ = self.__EQ + piece
+                print(c)
+            else:
+                piece = str(c) + "x^" + str(self.__len -i-1) 
+                self.__EQ = self.__EQ + piece
+                print(c)
+            i += 1
+        print(self.__EQ)
+        
+from Polynomial import *
+func = []
 Funky = [-2,5,-3,-1]
 usr = input("Enter all coeffecients wihtout proceeding 0s: ")
 usr = str(usr)
@@ -8,15 +33,17 @@ try:
     x2 = float(x2)
 except ValueError:
     print("Enter a value for x1, and x2")
+    exit()
     
-digits = usr.strip("()[]{}")
+digits = usr.strip("()[]{}+")
 floaty = digits.split(',')
-func = []
+
 for c in floaty:
     try:
         func.append(float(c))
     except ValueError:
-        print("Please enter coeffecients seperated by commas...")
+        print("Please enter NUMBERS seperated by commas...")
         exit()
 print(func)
-print(polynomial)
+test = Polynomial(func)
+test.polycraft()
