4b8b1d78a5bf0698365c466053be0230e0b6dc65
MN993647 <993647@pdsb.net>
2025-01-16 09:43:15 -0500

    Update IVT.py

--- IVT.py
+++ IVT.py
@@ -7,7 +7,7 @@ class Polynomial:
         self.__EQ = ""
         self.__derive = []
        
-    def polycraft(self):
+    def craft(self):
         i = 0
         for c in self.__func:
            
@@ -52,7 +52,7 @@ class Polynomial:
             i += 1
         return -1
    
-    def polysolve(self, x, y):
+    def solve(self, x, y):
         i = 0
         val = 0.0
         while i < self.__len:
@@ -61,17 +61,17 @@ class Polynomial:
             i += 1
         return val - y
    
-    def polyderive(self, funky):
+    def derivative(self):
         derive = []
         i = 0
         while i < self.__len:
             exp = self.__len - i - 1
-            if exp > 0 and funky[i] != 0:
-                derive.append(funky[i]*exp)
+            if exp > 0 and self.__func[i] != 0:
+                derive.append(self.__func[i]*exp)
             i += 1
         return derive
    
-    def derivative(self, der, x):
+    def derive(self, der, x):
         val = 0.0
         for i in range(len(der)):
             exp = len(der) - i - 1
@@ -81,108 +81,114 @@ class Polynomial:
     def f(self, x):
         i = 0
         y = 0.0
-        if self.get_order() == 0:
-            return "Every number from positive infinity to negative infinity"
-        elif self.get_order() > 0:
-            for c in self.__func:
-                if i == self.__len-1:
-                    y = y + c
-                elif i == self.__len-2:
-                    y = x*c + y
-                else:
-                    y = x**(self.__len-(i+1))*c+y
-                i += 1
-            return y
+        for c in self.__func:
+            if i == self.__len-1:
+                y = y + c
+            elif i == self.__len-2:
+                y = x*c + y
+            else:
+                y = x**(self.__len-(i+1))*c+y
+            i += 1
+        return y
    
-    def polyfind__x(self, y):
-        x = 0  
-        valid = 0.001  
-        Max = 100  
+    def x(self, y, x1, x2): 
+        valid = 0.001    
         test = 0
         r = None
-        r1 = None
-        r2 = None
-        der = self.polyderive(self.__func)
+        der = self.derivative(self.__func)
+        
+        while test < x2:
+            fx = self.solve(x1, y)
+            fx_pr = self.derive(der, x1)  
+
+            if fx_pr == 0:
+                print("Math Error")
+                break
+               
+            new_x = x1 - fx / fx_pr
+            if abs(new_x - x1) < valid:
+                r = new_x
+                break
+
+            x1 = new_x
+            test += 1
+        return r
+
+   
+    def findZero(self, _x1, _x2):
         deg = self.get_order()
-       
+        
+        if _x1 == _x2:
+            return("The x values must be different")
+        elif _x1 > _x2:
+            x1 = _x2
+            x2 = _x1
+        else:
+            x1 = _x1
+            x2 = _x2
+        
         if deg == -1:
-            print("No function found")
-            exit()
+            return "No function found"
+            
         elif deg == 0:
-            r = ("Every number from positive infinity to negative infinity")
+            r = self.__func[self.__len-1]
+                
         elif deg == 1:
-            r = (x - self.__func[self.__len-1])/der[0]
+            r = (0 - self.__func[self.__len-1])/der[0]
+            
         elif deg == 2:
             a = self.__func[self.__len-3]
             b = self.__func[self.__len-2]
-            c = self.__func[self.__len-1] - y
+            c = self.__func[self.__len-1] 
            
             Qf = (b**2 - 4*a*c)
-            print(Qf)
             if Qf > 0:
-                r1 = (-1*b + math.sqrt(Q))/(2*a)
-                r2 = (-1*b - math.sqrt(Q))/(2*a)
+                r1 = (-1*b + math.sqrt(Qf))/(2*a)
+                r2 = (-1*b - math.sqrt(Qf))/(2*a)
             elif Qf == 0:
                 r = (-1*b)/(2*a)
                 if r == 0:
                     r = abs(r)
-        elif deg > 2:
-            while test < Max:
-                fx = self.polysolve(x, y)
-                fx_pr = self.derivative(der, x)  
+            else:
+                return "No root found..."
+            
+        elif deg > 0:
+            der = self.derivative(self.__func)
 
-                if fx_pr == 0:
-                    print("Math Error")
-                    break
-               
-                new_x = x - fx / fx_pr
-               
-                if abs(new_x - x) < valid:
-                    r = new_x
-                    break
-
-                x = new_x
-                test += 1
-
-        if r is None and (r1 is None or r2 is None):
-            print("Not Found")
-        elif r2 != None:
-            return r1, r2
-        else:
-            return r
-   
-    def polyzeroes(self, x1, x2):  
-        root = None
-        root2 = None
-        der = self.polyderive(self.__func)
-        deg = self.get_order()
-       
-        if deg == -1:
-            print("No function found")
-            exit()
-        elif deg == 0:
-            r = ("Every number from positive infinity to negative infinity")
-        elif deg == 1:
-            r = (x - self.__func[self.__len-1])/der[0]
-        elif deg == 2:
-            a = self.__func[0]
-            b = self.__func[1]
-            c = self.__func[2] - y
-           
-            Qf = (b**2 - 4*a*c)/a
-            if Qf > 0:
-                r2 = -1*b + math.sqrt(Q)
-                r2 = -1*b - math.sqrt(Q)
-               
-        elif deg > 2:
-            pass
-
-        if root is None:
-            print("Not Found")
-        elif root2 != None:
-            return root1, root2
-        else:
-            return root
-        
+            
+             
     def __str__(self):
         return f"{self.__EQ}"
+
+#from Polynomial import *
+func = []
+Funky = [-2,5,-3,-1]
+usr = input("Enter all coeffecients wihtout proceeding 0s: ")
+usr = str(usr)
+x1 = input("Please input a point before the root in the function: ")
+x2 = input("Please input a point after the root in the function: ")
+try:
+    x1 = float(x1)
+    x2 = float(x2)
+except ValueError:
+    print("Enter a value for x1, and x2")
+    exit()
+    
+digits = usr.strip("()[]{}+")
+floaty = digits.split(',')
+
+for c in floaty:
+    try:
+        func.append(float(c))
+    except ValueError:
+        print("Please enter NUMBERS seperated by commas...")
+        exit()
+
+test = Polynomial(func)
+test.craft()
+
+print("Equation: ", test)
+print("Derivative: ", test.derivative())
+print("Find y: ", test.f(0))
+print("Find 0: ", test.findZero(x1,x2))
+
